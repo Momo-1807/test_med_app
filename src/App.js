1a3df6aae566a516e64b75764fb7e82834c57a7f
Momo-1807 <cse17021@gmail.com>
2024-08-03 10:21:59 -0400

    added landing page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
 
 // Function component for the main App
 function App() {
@@ -20,6 +21,7 @@ function App() {
 
           {/* Set up the Routes for different pages */}
           <Routes>
+          <Route path="/" element={<Landing_Page />} />
             {/* Define individual Route components for different pages */}
           </Routes>
         </BrowserRouter>
